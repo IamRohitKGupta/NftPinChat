1ed7838703adf07dd8a0a6660f4a8e266fe13e02
Rohit Kumar Gupta <rohit_32@live.com>
2023-07-19 11:20:20 +0530

    sockets

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const http = require('http'); // Import the built-in http module
+const socketIO = require('socket.io'); // Import Socket.IO
 
 const app = express();
 app.use(bodyParser.json());
+
+const server = http.createServer(app); // Create an HTTP server instance
+const io = socketIO(server); // Pass the server to Socket.IO
 
 // Define the allowed origin
 const allowedOrigin = 'https://nftpin.xyz';
@@ -36,6 +41,9 @@ app.post('/chats', (req, res) => {
     chats.shift();
   }
 
+  // Emit the new chat message to all connected clients
+  io.emit('newChatMessage', newChat);
+
   res.status(201).json({ message: 'Chat message added successfully' });
 });
 
@@ -45,6 +53,19 @@ app.get('/chats', (req, res) => {
 });
 
 const port = 8000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+// Socket.IO event for new client connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Send the current chats to the newly connected client
+  socket.emit('chats', chats);
+
+  // Handle disconnection event (optional)
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
